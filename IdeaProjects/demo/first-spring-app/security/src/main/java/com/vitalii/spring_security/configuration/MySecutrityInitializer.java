a5c5da401558cbb35b8b9adc380492cfd2db143f
Vitalii Sumka <61744465+vitaliksumka@users.noreply.github.com>
2022-01-14 15:21:19 +0100

    Delete IdeaProjects/demo/first-spring-app/security directory

--- IdeaProjects/demo/first-spring-app/security/src/main/java/com/vitalii/spring_security/configuration/MySecutrityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.vitalii.spring_security.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class MySecutrityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
